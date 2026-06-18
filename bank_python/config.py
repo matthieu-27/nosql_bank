446de08386614c5783daace4d7c1bf34a3adbc71
matthieu-27 <matthieu27@pm.me>
2026-06-18 11:04:55 +0200

    feat: add config

---
+++ bank_python/config.py
@@ -0,0 +1,2 @@
+MONGODB_URI = "mongodb+srv://Cluster84008:W0xze0VceXZy@cluster84008.nyc25kd.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "banque"  # Remplacez par le nom de votre base de données Atlas
